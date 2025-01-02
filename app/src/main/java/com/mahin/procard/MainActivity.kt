422db68a68d52441335f59095ba199f104f6bd4e
memahin <me.mahin1@gmail.com>
2025-01-03 03:11:17 +0600

    Mahin

--- app/src/main/java/com/mahin/procard/MainActivity.kt
+++ app/src/main/java/com/mahin/procard/MainActivity.kt
@@ -1,37 +1,44 @@
 package com.mahin.procard
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.mahin.procard.ui.theme.ProCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,6 +55,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateProCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -63,20 +73,112 @@ fun CreateProCard() {
                 defaultElevation = 4.dp
             )
         ) {
-            Surface(
-                modifier = Modifier.size(150.dp)
-                    .padding(5.dp),
-                shape = CircleShape,
-                border = BorderStroke(0.5.dp,Color.LightGray),
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f))
+            Column(modifier = Modifier
+                .height(600.dp)
+                .width(400.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally)
             {
-                Image(painter = painterResource(id = R.drawable.propic),
-                    contentDescription = "profile image",
-                    modifier = Modifier.size(135.dp),
-                    contentScale = ContentScale.Crop)
+                CreateImageProfile()
+                Divider()
+                CreateInfo()
+                Button(
+                    onClick = {
+                        buttonClickedState.value = !buttonClickedState.value
+
+
+
+                    }
+                ) {
+                    Text(text = "Protfolio",
+                        style = MaterialTheme.typography.labelLarge)
+                }
+                if(buttonClickedState.value){
+                    Content()
+                }else{
+                    Box() {  }
+                }
 
             }
         }
+
+
+    }
+}
+
+@Composable
+fun Content(){
+    Box(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .padding(5.dp)){
+        Surface(modifier = Modifier
+            .padding(3.dp)
+            .fillMaxHeight()
+            .fillMaxWidth(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = LightGray)
+        ) {
+
+            Protfolio(data = listOf("Project 1",
+                "Project 2",
+                "Project 3",
+                "Project 4"))
+
+        }
+    }
+
+}
+
+@Composable
+fun Protfolio(data: List<String>) {
+    LazyColumn {
+        items(data){
+            item ->
+            Text(item)
+        }
+    }
+
+}
+
+@Composable
+private fun CreateInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Rafiul Islam Mahin",
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Android App Developer",
+            modifier = Modifier.padding(3.dp)
+        )
+
+        Text(
+            text = "@AndroidLearner",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.labelLarge
+        )
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+    )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.propic),
+            contentDescription = "profile image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
